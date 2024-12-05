58ac5f09cf123aef8ca4c982129c19e8c7489a89
Abdur Razzak <abdurrazzakyeamin@gmail.com>
2024-12-06 02:48:34 +0600

    user added

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-require("dotenv").config()
-const express = require('express')
-const cors = require('cors');
-const app = express()
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.6s1yf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -17,24 +17,45 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
-const userCollection = client.db("CineVerseDB").collection("users")
+const userCollection = client.db("CineVerseDB").collection("users");
+const movieCollection = client.db("CineVerseDB").collection("movies");
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
 
-    app.post('/users', async (req, res) => {
+    // create user
+    app.post("/users", async (req, res) => {
       const newUser = req.body;
       const result = await userCollection.insertOne(newUser);
-      res.send(newUser )
-})
+      res.send(result);
+    });
 
+    // add movies to db
+    app.post('/movies', async (req, res) => {
+      const newMovie = req.body;
+      const result = await movieCollection.insertOne(newMovie);
+      console.log(result);
+      res.send(result)
+    })
 
+    // get all movies
+    app.get('/movies', async (req, res) => {
+      const result = await movieCollection.find().toArray();
+      res.send(result)
+    })
 
+    // get one
+    app.get('/movies/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await movieCollection.findOne(query);
+      res.send(result);
+    })
 
 
 
@@ -42,7 +63,9 @@ async function run() {
 
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -50,10 +73,10 @@ async function run() {
 }
 run().catch(console.dir);
 
-app.get('/', (req, res) => {
-  res.send('CineVerse Server')
-})
+app.get("/", (req, res) => {
+  res.send("CineVerse Server");
+});
 
 app.listen(port, () => {
-  console.log(`This server is running on port ${port}`)
-})
+  console.log(`This server is running on port ${port}`);
+});
