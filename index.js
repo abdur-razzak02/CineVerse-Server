62643f5da4a1b9b7f3416ddf4e5082f390747359
Abdur Razzak <abdurrazzakyeamin@gmail.com>
2024-12-07 23:52:46 +0600

    filter featured movie added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -8,7 +9,6 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.6s1yf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -52,10 +52,21 @@ async function run() {
     // get one
     app.get('/movies/:id', async (req, res) => {
       const id = req.params.id;
+      console.log(id);
       const query = { _id: new ObjectId(id) };
       const result = await movieCollection.findOne(query);
+     
       res.send(result);
     })
+
+    // get filtered based (featured-movies)
+    app.get('/featured-movies/featured', async (req, res) => {
+      const query = { rating: { $gt: 3 }}
+      const result = await movieCollection.find(query).sort({ rating: -1 }).limit(6).toArray()
+      res.send(result)
+    })
+
+
 
 
 
@@ -74,7 +85,7 @@ async function run() {
 run().catch(console.dir);
 
 app.get("/", (req, res) => {
-  res.send("CineVerse Server");
+  res.send("CineVerse Movie Server");
 });
 
 app.listen(port, () => {
