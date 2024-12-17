e1d215578417145dacfd5c80fc26d9f191e588cb
Abdur Razzak <abdurrazzakyeamin@gmail.com>
2024-12-18 00:11:36 +0600

    code updated

---
+++ README.md
@@ -0,0 +1,23 @@
+## CineVerse
+
+CineVerse is an innovative, user-friendly movie streaming website designed to cater to movie enthusiasts around the globe. It offers a vast library of films across diverse genres, eras, and languages, making it the perfect hub for anyone who loves cinema. With an intuitive interface and cutting-edge features, CineVerse provides a seamless viewing experience for users of all ages.
+
+[CineVerse live link](cineverse-9ca24.web.app)
+
+### Features 
+Frontend features
+- Extensive Movie Library
+- Search Filtering
+- Movie Details
+- Responsive Design
+- User Accounts 
+- User Favourite List
+
+
+
+### Technical Highlights
+
+**Frontend:** React.js with TailwindCSS and DaisyUI
+**Backend:** Node.js and Express.js for fast and secure API endpoints.
+**Database:** MongoDB for efficient data storage and retrieval.
+**Authentication:** Firebase login and signup systems.
--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
+// app.use(cors({origin:['https://cineverse-9ca24.web.app']}));
 app.use(cors());
 app.use(express.json());
 
@@ -22,6 +23,7 @@ const client = new MongoClient(uri, {
 
 const userCollection = client.db("CineVerseDB").collection("users");
 const movieCollection = client.db("CineVerseDB").collection("movies");
+const favoriteCollection = client.db("CineVerseDB").collection("favorites");
 
 async function run() {
   try {
@@ -36,88 +38,144 @@ async function run() {
     });
 
     // add movies to db
-    app.post('/movies', async (req, res) => {
+    app.post("/movies", async (req, res) => {
       const newMovie = req.body;
       const result = await movieCollection.insertOne(newMovie);
-      console.log(result);
-      res.send(result)
-    })
+      res.send(result);
+    });
 
     // get all movies
-    app.get('/movies', async (req, res) => {
+    app.get("/movies", async (req, res) => {
       const result = await movieCollection.find().toArray();
-      res.send(result)
-    })
+      res.send(result);
+    });
 
     // get one
-    app.get('/movies/:id', async (req, res) => {
+    app.get("/movies/:id", async (req, res) => {
       const id = req.params.id;
-      console.log(id);
       const query = { _id: new ObjectId(id) };
       const result = await movieCollection.findOne(query);
-     
+
+      res.send(result);
+    });
+
+    // get filtered based (featured-movies)
+    app.get("/featured-movies/featured", async (req, res) => {
+      const query = { rating: { $gt: 3 } };
+      const result = await movieCollection
+        .find(query)
+        .sort({ rating: -1 })
+        .limit(6)
+        .toArray();
+      res.send(result);
+    });
+
+    // get filtered based (action-movies)
+    app.get("/action-movies/action", async (req, res) => {
+      const query = { genre: { $eq: "Action" } };
+      const result = await movieCollection.find(query).limit(20).toArray();
+      res.send(result);
+    });
+
+    // get filtered based (romantic-movies)
+    app.get("/romantic-movies/romantic", async (req, res) => {
+      const query = { genre: { $eq: "Romantic" } };
+      const result = await movieCollection.find(query).limit(20).toArray();
+      res.send(result);
+    });
+
+    // get filtered based (comdedy-movies)
+    app.get("/comedy-movies/comedy", async (req, res) => {
+      const query = { genre: { $eq: "Comedy" } };
+      const result = await movieCollection.find(query).limit(20).toArray();
+      res.send(result);
+    });
+
+    // get filtered based (drama-movies)
+    app.get("/drama-movies/drama", async (req, res) => {
+      const query = { genre: { $eq: "Drama" } };
+      const result = await movieCollection.find(query).limit(20).toArray();
+      res.send(result);
+    });
+
+    // get filtered based (horror-movies)
+    app.get("/horror-movies/horror", async (req, res) => {
+      const query = { genre: { $eq: "Horror" } };
+      const result = await movieCollection.find(query).limit(20).toArray();
+      res.send(result);
+    });
+
+    // get filtered based (science-fiction-movies)
+    app.get("/science-fiction-movies/fiction", async (req, res) => {
+      const query = { genre: { $eq: "Science Fiction" } };
+      const result = await movieCollection.find(query).limit(20).toArray();
+      res.send(result);
+    });
+
+    // get filtered based (thriller-movies)
+    app.get("/thriller-movies/thriller", async (req, res) => {
+      const query = { genre: { $eq: "Thriller" } };
+      const result = await movieCollection.find(query).limit(20).toArray();
+      res.send(result);
+    });
+
+    // add to favorite
+    app.post('/favorites', async (req, res) => {
+      const { email, title } = req.body;
+      // const existingFavorite = await favoriteCollection.findOne({ email, title });
+      // if (existingFavorite) {
+      //     return res.status(400).json({ message: 'Movie already in favorites.' });
+      // }
+      const result = await favoriteCollection.insertOne({ email, title });
+      console.log(result);
+      
       res.send(result);
     })
 
-    // get filtered based (featured-movies)
-    app.get('/featured-movies/featured', async (req, res) => {
-      const query = { rating: { $gt: 3 }}
-      const result = await movieCollection.find(query).sort({ rating: -1 }).limit(6).toArray()
-      res.send(result)
-    })
 
-    // get filtered based (action-movies)
-    app.get('/action-movies/action', async (req, res) => {
-      const query = { genre: {$eq: "Action"}}
-      const result = await movieCollection.find(query).limit(20).toArray()
-      res.send(result)
-    })
+    // get favorites
+    app.get('/favorites', async (req, res) => {
+      const { email } = req.query;
+  
+      if (!email) {
+          return res.status(400).json({ error: 'Email is required.' });
+      }
+      const favorites = await favoriteCollection.find({ email }).toArray();
+      console.log(favorites);
+      res.send(favorites);
+      
+  });
+  
+  // module.exports = router;
+  
 
-    // get filtered based (romantic-movies)
-    app.get('/romantic-movies/romantic', async (req, res) => {
-      const query = { genre: {$eq: "Romantic"}}
-      const result = await movieCollection.find(query).limit(20).toArray()
-      res.send(result)
-    })
 
-    // get filtered based (comdedy-movies)
-    app.get('/comedy-movies/comedy', async (req, res) => {
-      const query = { genre: {$eq: "Comedy"}}
-      const result = await movieCollection.find(query).limit(20).toArray()
-      res.send(result)
-    })
+    // update details
+    // app.put("/movies/:id", async (req, res) => {
+    //   const id = req.params.id;
+    //   const filter = { _id: new ObjectId(id) };
+    //   const options = { upsert: true };
+    //   const updateMovie = req.body;
+    //   const movie = {
+    //     $set: {
+    //       title: updateMovie.title,
+    //       duration: updateMovie.chef,
+    //       genre: updateMovie.genre,
+    //       poster: updateMovie.poster,
+    //       rating: updateMovie.rating,
+    //       releaseYear: updateMovie.releaseYear,
+    //       summary: updateMovie.summary,
+    //     },
+    //   };
+    //   const result = await movieCollection.updateOne(filter, movie, options);
+    //   res.send(result);
+    // });
 
-    // get filtered based (drama-movies)
-    app.get('/drama-movies/drama', async (req, res) => {
-      const query = { genre: {$eq: "Drama"}}
-      const result = await movieCollection.find(query).limit(20).toArray()
-      res.send(result)
-    })
+    // add to favourite
 
-    // get filtered based (horror-movies)
-    app.get('/horror-movies/horror', async (req, res) => {
-      const query = { genre: {$eq: "Horror"}}
-      const result = await movieCollection.find(query).limit(20).toArray()
-      res.send(result)
-    })
+
+
     
-    // get filtered based (science-fiction-movies)
-    app.get('/science-fiction-movies/fiction', async (req, res) => {
-      const query = { genre: {$eq: "Science Fiction"}}
-      const result = await movieCollection.find(query).limit(20).toArray()
-      res.send(result)
-    })
-    
-    // get filtered based (thriller-movies)
-    app.get('/thriller-movies/thriller', async (req, res) => {
-      const query = { genre: {$eq: "Thriller"}}
-      const result = await movieCollection.find(query).limit(20).toArray()
-      res.send(result)
-    })
-
-
-
-
 
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
